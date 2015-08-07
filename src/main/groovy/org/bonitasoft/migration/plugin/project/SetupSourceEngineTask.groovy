e910a45c470a758041440788188494849c89ca5e
Baptiste Mesta <baptiste.mesta@bonitasoft.com>
2015-08-07 17:40:32 +0200

    use setter instead of direct set on the field

--- src/main/groovy/org/bonitasoft/migration/plugin/project/SetupSourceEngineTask.groovy
+++ src/main/groovy/org/bonitasoft/migration/plugin/project/SetupSourceEngineTask.groovy
@@ -11,10 +11,10 @@ class SetupSourceEngineTask extends JavaExec {
 
     @Override
     void exec() {
-        description = "Setup the engine in order to run migration tests on it."
-        main = "org.bonitasoft.migration.filler.FillerRunner"
+        setDescription "Setup the engine in order to run migration tests on it."
+        setMain "org.bonitasoft.migration.filler.FillerRunner"
         args "org.bonitasoft.migration.FillBeforeMigratingTo${project.isSP ? 'SP' : ''}" + project.bonitaVersionUnderScore
-        setClasspath(project.sourceSets.main.runtimeClasspath)
+        setClasspath project.sourceSets.main.runtimeClasspath
         super.exec()
     }
 }
