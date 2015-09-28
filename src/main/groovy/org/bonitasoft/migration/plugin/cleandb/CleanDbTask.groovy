2fe72fae8f1794c0ecd6bb5b11ddb20535d67d2c
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2015-09-28 14:30:30 +0200

    BS-14267, add specific user deletion for oracle 12c

--- src/main/groovy/org/bonitasoft/migration/plugin/cleandb/CleanDbTask.groovy
+++ src/main/groovy/org/bonitasoft/migration/plugin/cleandb/CleanDbTask.groovy
@@ -17,6 +17,7 @@ package org.bonitasoft.migration.plugin.cleandb
 import groovy.sql.Sql
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
+
 /**
  * @author Baptiste Mesta
  */
@@ -41,9 +42,6 @@ class CleanDbTask extends DefaultTask {
             loader1.addURL(file.toURI().toURL())
         }
         Sql.class.getClassLoader().loadClass(properties.dbdriverClass)
-
-        logger.info "dbvendor business data dialect is $properties.dbvendor"
-
         if (properties.dbvendor.equals("oracle")) {
             logger.info "cleaning oracle database $properties.dbuser"
             if (properties.dbRootUser == null || properties.dbRootUser.isEmpty() || properties.dbRootPassword == null || properties.dbRootPassword.isEmpty()) {
@@ -51,20 +49,72 @@ class CleanDbTask extends DefaultTask {
             }
             def props = [user: properties.dbRootUser, password: properties.dbRootPassword] as Properties
             def Sql sql = Sql.newInstance(properties.dburl, props, properties.dbdriverClass)
-            try {
-                sql.executeUpdate("DROP user " + properties.dbuser + " cascade");
-            } catch (Exception e) {
-                logger.info "can't drop database, maybe it did not exist, cause = "
-                e.printStackTrace()
 
-            }
-            //do not use gstring because it create prepared statement
-            sql.executeUpdate("CREATE USER " + properties.dbuser + " IDENTIFIED BY " + properties.dbpassword);
-            sql.executeUpdate("GRANT connect, resource TO " + properties.dbuser + " IDENTIFIED BY " + properties.dbpassword);
-            sql.executeUpdate("GRANT select ON sys.dba_pending_transactions TO " + properties.dbuser);
-            sql.executeUpdate("GRANT select ON sys.pending_trans\$ TO " + properties.dbuser);
-            sql.executeUpdate("GRANT select ON sys.dba_2pc_pending TO " + properties.dbuser);
-            sql.executeUpdate("GRANT execute ON sys.dbms_system TO " + properties.dbuser);
+            sql.execute("""
+declare
+  v_count number;
+  v_banner varchar2(50) := 'Oracle Database 12c%';
+
+  cursor c1 is
+    select s.sid, s.serial#, s.username """
+                    + ''' from gv$session s ''' + """
+    where s.type != 'BACKGROUND' and (upper(s.username) = upper('${properties.dbuser}') OR upper(s.username) = upper('${
+                properties.dbuser
+            }_bdm'));
+
+  cursor c2 is
+"""
+                    + ''' select v.banner from v$version v; '''
+                    + """
+                    begin
+  -- disconnect sessions
+  for session_rec in c1
+  loop
+    execute immediate 'ALTER SYSTEM DISCONNECT SESSION session_rec.sid,session_rec.serial# IMMEDIATE';
+  end loop;
+
+  -- for oracle 12c
+  for cur_banner in c2 loop
+    if cur_banner.banner LIKE v_banner
+    then
+      execute immediate 'alter session set "_ORACLE_SCRIPT"=true';
+    end if;
+  end loop;
+
+
+  -- drop user if exists
+  select count(1) into v_count from dba_users where upper(username) = upper('${properties.dbuser}');
+  if v_count != 0
+  then
+    execute immediate 'drop user ${properties.dbuser} cascade';
+  end if;
+
+  select count(1) into v_count from dba_users where upper(username) = upper('${properties.dbuser}_bdm');
+  if v_count != 0
+  then
+    execute immediate 'drop user ${properties.dbuser}_bdm cascade';
+  end if;
+
+  -- recreate user
+  execute immediate 'CREATE USER ${properties.dbuser} IDENTIFIED BY ${properties.dbpassword}';
+  execute immediate 'ALTER USER ${properties.dbuser} QUOTA 300M ON USERS';
+  execute immediate 'GRANT connect, resource TO ${properties.dbuser}';
+  execute immediate 'GRANT select ON sys.dba_pending_transactions TO ${properties.dbuser}';
+  execute immediate 'GRANT select ON sys.pending_trans\$ TO ${properties.dbuser}';
+  execute immediate 'GRANT select ON sys.dba_2pc_pending TO ${properties.dbuser}';
+  execute immediate 'GRANT execute ON sys.dbms_system TO ${properties.dbuser}';
+
+  execute immediate 'CREATE USER ${properties.dbuser}_bdm IDENTIFIED BY ${properties.dbpassword}';
+  execute immediate 'ALTER USER ${properties.dbuser}_bdm QUOTA 300M ON USERS';
+  execute immediate 'GRANT connect, resource TO ${properties.dbuser}_bdm';
+  execute immediate 'GRANT select ON sys.dba_pending_transactions TO ${properties.dbuser}_bdm';
+  execute immediate 'GRANT select ON sys.pending_trans\$ TO ${properties.dbuser}_bdm';
+  execute immediate 'GRANT select ON sys.dba_2pc_pending TO ${properties.dbuser}_bdm';
+  execute immediate 'GRANT execute ON sys.dbms_system TO ${properties.dbuser}_bdm';
+end;
+"""
+            )
+
         } else if (properties.dbvendor.equals("sqlserver")) {
             logger.info "cleaning sqlserver database $properties.dburl"
             if (properties.dbRootUser == null || properties.dbRootUser.isEmpty() || properties.dbRootPassword == null || properties.dbRootPassword.isEmpty()) {
